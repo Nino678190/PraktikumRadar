a9bc06b7b26d0d4add4b115f6a63c5380fc63282
Nino678190 <superwauzi@gmx.de>
2025-04-03 22:39:40 +0200

    Füge index.html, script.js und style.css hinzu; implementiere Header, Footer und Home-Seite

--- script.js
+++ script.js
@@ -8,14 +8,58 @@ client
 
 const databases = new Databases(client); // Create a new instance of the Databases class
 
-databases.listDocuments( // List all documents in a collection
-    "67eebf55000c4fcc2eac",
-    "67eebf7900353b1d71ca",
-    [
-        Query.equal('title', 'Avatar') // Query to filter documents
-    ]
-).then(function (response) {
-    console.log(response); // Log the response
-}, function (error) {
-    console.log(error); // Log any errors
-});
+function getData(){
+    databases.listDocuments( // List all documents in a collection
+        "67eebf55000c4fcc2eac",
+        "67eebf7900353b1d71ca",
+        [
+            Query.equal('title', 'Avatar') // Query to filter documents
+        ]
+    ).then(function (response) {
+        console.log(response); // Log the response
+    }, function (error) {
+        console.log(error); // Log any errors
+    });
+}
+
+function createHeader(){
+    const header = document.createElement('header'); // Create a header element
+    header.className = 'header'; // Set the class name
+    header.innerHTML = `
+    <section>
+        <a onclick="showHome()" class="logo">
+            <img src="assets/logo.png" alt="Logo">
+        </a>
+        <nav class="nav">
+            <a onclick="showHome()">Home</a>
+            <a onclick="showPraktikas()">Praktikas</a>
+        </nav>
+    </section>
+    `; // Set the inner HTML
+    return header; // Return the header element
+}
+
+function createFooter(){
+    const footer = document.createElement('footer'); // Create a footer element
+    footer.className = 'footer'; // Set the class name
+    footer.innerHTML = `
+    <section>
+        <p>All rights reserved &copy; ${new Date().getFullYear()} by <a href="https://github.com/Nino678190">Nino678190</a></p>
+    </section>
+    `; // Set the inner HTML
+    return footer; // Return the footer element
+}
+
+function showPraktikas(){}
+
+function createHome(){
+    const main = document.createElement('main'); // Create a main element
+    main.className = 'home';
+    main.innerHTML = `
+    <section>
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the home page content.</p>
+    </section>
+    `; // Set the inner HTML
+    return main; // Return the main element
+}
