0ac9c2b2013eb509660e9cd178ae2c873e157c8f
Nino678190 <superwauzi@gmx.de>
2025-04-03 23:20:33 +0200

    Füge Filterfunktionalität hinzu und aktualisiere die Datenabfrage in script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const { Client, Databases } = require("appwrite"); // Importing the Client and Databases classes from the appwrite package
+const { Client, Databases, Query } = require("appwrite"); // Importing the Client and Databases classes from the appwrite package
 const client = new Client(); // Create a new instance of the Client class
 
 client
@@ -7,18 +7,6 @@ client
 ;
 
 const databases = new Databases(client); // Create a new instance of the Databases class
-
-function getData(){
-    databases.listDocuments( // List all documents in a collection
-        "67eebf55000c4fcc2eac",
-        "67eebf7900353b1d71ca",
-    ).then(function (response) {
-        console.log(response); // Log the response
-        return response; // Return the response
-    }, function (error) {
-        console.log(error); // Log any errors
-    });
-}
 
 function createHeader(){
     const header = document.createElement('header'); // Create a header element
@@ -47,6 +35,75 @@ function createFooter(){
     `; // Set the inner HTML
     return footer; // Return the footer element
 }
+
+function showFilter(){
+    const filter = document.createElement('div'); // Create a filter element
+    filter.className = 'filter'; // Set the class name
+    filter.innerHTML = `
+    <section>
+        <h2>Filter</h2>
+        <form>
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name">
+            <label for="ort">Ort:</label>
+            <input type="text" id="ort" name="ort">
+            <label for="berufsfeld">Berufsfeld:</label>
+            <input type="text" id="berufsfeld" name="berufsfeld">
+            <label for="Beginn">Beginn:</label>
+            <input type="date" id="beginn" name="beginn">
+            <label for="Dauer">Dauer:</label>
+            <input type="number" id="dauer" name="dauer">
+            <button type="submit" onclick="displayPraktikas()">Filter</button>
+        </form>
+    </section>
+    `; // Set the inner HTML
+    return filter; // Return the filter element
+}
+
+function getFilterData(){
+    let name = document.getElementById('name').value; // Get the name value
+    let ort = document.getElementById('ort').value; // Get the ort value
+    let berufsfeld = document.getElementById('berufsfeld').value; // Get the berufsfeld value
+    let beginn = document.getElementById('beginn').value; // Get the beginn value
+    let dauer = document.getElementById('dauer').value; // Get the dauer value
+    // Check if the fields are empty and set them to null
+    if (name === '') name = null;
+    if (ort === '') ort = null;
+    if (berufsfeld === '') berufsfeld = null;
+    if (beginn === '') beginn = null;
+    if (dauer === '') dauer = null;
+    // Return the filter data
+    return { name, ort, berufsfeld, beginn, dauer }; // Return the filter data
+}
+
+function getData() {
+    const filter = getFilterData(); // Get the filter data
+    const name = filter.name; // Get the name from the filter
+    const ort = filter.ort; // Get the ort from the filter
+    const berufsfeld = filter.berufsfeld; // Get the berufsfeld from the filter
+    const beginn = filter.beginn; // Get the beginn from the filter
+    const dauer = filter.dauer; // Get the dauer from the filter
+    // Create a query object
+    const query = [];
+    if (name) query.push(Query.contains('Name', name)); // If name is not null, add it to the query
+    if (ort) query.push(Query.contains('Ort', ort)); // If ort is not null, add it to the query
+    if (berufsfeld) query.push(Query.contains('Berufsfeld', berufsfeld)); // If berufsfeld is not null, add it to the query
+    if (beginn) query.push(Query.lessThanEqual('Beginn', beginn)); // If beginn is not null, add it to the query
+    if (dauer) query.push(Query.lessThanEqual('Dauer', dauer)); // If dauer is not null, add it to the query
+    databases.listDocuments( // List all documents in a collection
+        "67eebf55000c4fcc2eac",
+        "67eebf7900353b1d71ca",
+        [ // Query parameters
+            query
+        ]
+    ).then(function (response) {
+        console.log(response); // Log the response
+        return response; // Return the response
+    }, function (error) {
+        console.log(error); // Log any errors
+    });
+}
+
 
 function displayPraktikas(){
     const data = getData(); // Get data from the database
